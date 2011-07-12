47993e621e9a65c44426dfb6773b3f55ef150d36
lies <lies@i.ua>
2011-07-12 11:39:18 +0300

    remove all

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Lies::Application.config.secret_token = '51b3949961671297baf044d991b991875b63fad5da841e87d838c405efab0e8aa74deec0a6902104201450a24bf36c72d1517258451cadba3260242362362712'
